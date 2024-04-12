1be7a4e02112511392136ac6cdcbd08adb1a6b5e
Nikhilrolawan <101587890+Nikhilrolawan@users.noreply.github.com>
2024-04-12 23:47:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ if(selected=='Heart_Risk_Prediction'):
   st.title('Heart Risk Prediction Using ML')
   age=st.text_input('Age')
   sex=st.text_input('sex type(male=1 or female=0)')
-  cp=st.text_input('chest pain type')
+  cp=st.text_input('chest pain (yes=1 or no=1'))
   trestbps=st.text_input('Resting BP')
   chol=st.text_input('Serum cholestrol(mg/dl)')
   fbs=st.text_input('Fasting blood sugar>120(mg/dl)')
@@ -19,7 +19,7 @@ if(selected=='Heart_Risk_Prediction'):
   thalach=st.text_input('Max heart rate achieved')
   exang=st.text_input('Exercise induced angina')
   oldpeak=st.text_input(' ST depression induced by exercise relative to rest')
-  slope=st.text_input('')
+  slope=st.text_input('the slope of the peak exercise ST segment')
   ca=st.text_input('Calcium level')
   thal=st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
   
